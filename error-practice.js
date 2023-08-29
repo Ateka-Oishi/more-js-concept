91a35ef63608badb90f91cd21e74d1399fd90803
Ateka Sultana <atekaoishi78@gmail.com>
2023-08-29 22:35:17 +0600

    some changes

--- error-practice.js
+++ error-practice.js
@@ -1,20 +1,28 @@
 function checkAge(){
-    // console.log('button-clicked');
     const ageField = document.getElementById('age');
     const ageText = ageField.value;
     const errorTag = document.getElementById('error');
+    
     try{
-       console.log(bBaria); 
+        const age = parseInt(ageText);
 
+        if(isNaN(age)){
+            throw "Please enter a number";
+        }
+        else if(age < 18){
+            throw "bacha kaccha not allowed";
+        }
+        else if(age > 30){
+            throw "murobbi ra aikhan aisen na "
+        }
+        errorTag.innerHTML = '';
     }
-
     catch(err){
-        console.log('ERROR:', err);
-        errorTag.innerHTML = 'Something wrong'
+        console.log('ERROR:', err)
+        errorTag.innerHTML = 'ERROR:' + err;
     }
-
-    finally(){
-
+    finally{
+        console.log('All done inside try catch')
     }
     console.log(11111);
 }
